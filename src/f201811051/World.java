5388c11fc144740cafe3404deb954c9c7a3cb279
Kim, Junghun <kebin10804@gmail.com>
2024-10-16 09:26:25 +0900

    five

--- src/f201811051/World.java
+++ src/f201811051/World.java
@@ -5,4 +5,8 @@ public class World {
     public static void run() {
         System.out.println("201811051");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
